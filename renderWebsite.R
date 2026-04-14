f49ffeb5ddd6432757ae745cff3cbf610d6b8389
doomfighter <daniel-koenigs@gmx.de>
2026-04-14 23:03:15 +0200

    queries for all used datasets finished, added render loop for map files

--- renderWebsite.R
+++ renderWebsite.R
@@ -6,7 +6,6 @@ library(tidyr)
 library(stringr)
 library(sf)
 library(quarto)
-library(leaflet)
 
 ## read data
 tracks = read.csv("./data/tracks.csv")
@@ -41,16 +40,95 @@ tableData = bind_rows(
   distinct() |>
   filter(!is.na(file) & str_detect(file, 'silence|red_legion.mp3', negate = TRUE)) |>
   arrange(file, mapSort) |>
-  group_by(campaignSong, file, youtubeCode) |>
-  summarise(maps = ifelse(sum(!is.na(map) > 0), paste0('<a href = ', map |> na.omit(), '.html>', mapNameDE |> na.omit(), '</a>', collapse = '<br>'), '')) |>
-  ungroup() |>
+  summarise(
+    maps = ifelse(sum(!is.na(map) > 0), paste0('<a href = ', map |> na.omit(), '.html>', mapNameDE |> na.omit(), '</a>', collapse = '<br>'), ''),
+    .by = c(campaignSong, file, youtubeCode)
+  ) |>
   mutate(
     campaignSong = campaignSong |> factor(labels = c('The Order of Dawn', 'The Breath of Winter', 'Shadow of the Phoenix')),
     youtubeLink = ifelse(youtubeCode == '', '', paste0('<a href="https://www.youtube.com/watch?', youtubeCode,'">Link</a>'))
   )
 
+mapSongs = maps |>
+  left_join(
+    tracks |> left_join(songs) |> select(id, file, youtubeCode),
+    by = join_by(openerId == id)
+  ) |>
+  left_join(
+    tracks |> left_join(songs) |> select(id, file, youtubeCode),
+    by = join_by(loopId == id),
+    suffix = c('Opener', 'Loop')
+  ) |>
+  select(-mapSort, -length, -campaign, -openerId, -loopId)
+
+locationSongs = locations |>
+  left_join(
+    maps |> select(map, length),
+    by = 'map'
+  ) |> left_join(
+    tracks |> left_join(songs) |> select(id, file, youtubeCode),
+    by = join_by(openerId == id)
+  ) |>
+  left_join(
+    tracks |> left_join(songs) |> select(id, file, youtubeCode),
+    by = join_by(loopId == id),
+    suffix = c('Opener', 'Loop')
+  ) |>
+  select(-openerId, -loopId) |>
+  
+  ## add circle polygons
+  rowwise() |>
+  mutate(
+    geometry = lapply(
+      0:359, function(d) { # d = degrees of circle
+        
+        point.x = sin(d / 360 * 2 * pi) * radius + x
+        point.y = cos(d / 360 * 2 * pi) * radius + y
+        
+        st_point(
+          c(
+            case_when(
+              x < 1 ~ 1,
+              x > length ~ length,
+              .default = point.x
+            ),
+            case_when(
+              y < 1 ~ 1,
+              y > length ~ length,
+              .default = point.y
+            )
+          )
+        )
+        
+      }
+    ) |>
+      st_as_sfc() |>
+      st_combine() |>
+      st_cast('POLYGON')
+  ) |>
+  ungroup() |>
+  
+  ## union circles with same songs
+  summarise(
+    geometry = st_union(geometry),
+    .by = c(map, fileOpener, youtubeCodeOpener, fileLoop, youtubeCodeLoop)
+  )
+
 # save data (as the global environment is not accessible within a quarto render)
-save(tableData, file = './data/data.Rdata')
+save(tableData, mapSongs, locationSongs, file = './data/data.Rdata')
 
 # render index and map files
+## index
 quarto_render(input = "index.qmd")
+
+## loop through all maps
+for (i in 1:2) { #nrow(maps)) {
+  quarto_render(
+    input = "mapTemplate.qmd",
+    output_file = paste0(maps[i, 'map'], '.html'),
+    execute_params = list(
+      map = maps[i, 'map'],
+      mapTitle = maps[i, 'mapNameDE']
+    )
+  )
+}
